2c699fab75d9e7e4eb252e9dde11b063caefa80e
JonathanSep <jonathan.sepulvedacelaya@gmail.com>
2026-02-03 18:41:52 -0300

    Debug: Agregar manejador de errores global en index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const multer = require('multer'); 
 
 dotenv.config();
 connectDB();
@@ -10,8 +11,8 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors()); // Permite peticiones de otros dominios
-app.use(express.json()); // Permite leer JSON del body (req.body)
+app.use(cors()); 
+app.use(express.json()); 
 
 // Rutas
 app.use('/api/products', productRoutes);
@@ -19,6 +20,24 @@ app.use('/api/products', productRoutes);
 // Ruta de prueba
 app.get('/', (req, res) => {
     res.send('API de Gigi Accesorios funcionando...');
+});
+
+app.use((err, req, res, next) => {
+    console.error("🔥 ERROR CAPTURADO EN EL INDEX:", JSON.stringify(err, null, 2));
+
+    if (err instanceof multer.MulterError) {
+        return res.status(400).json({ 
+            message: 'Error de subida (Multer)', 
+            error: err.code,
+            field: err.field
+        });
+    } else if (err) {
+        return res.status(500).json({ 
+            message: 'Error interno del servidor', 
+            error: err.message || err 
+        });
+    }
+    next();
 });
 
 const PORT = process.env.PORT || 4000;
